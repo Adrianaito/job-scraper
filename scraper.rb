dbc9c4e33362435ae5347f2a700d8c15f74d9062
Adriana Ito <ito.dri@gmail.com>
2021-03-07 18:33:52 +0900

    add url

--- scraper.rb
+++ scraper.rb
@@ -14,14 +14,17 @@ def scraper
       company: list.css('div.media-object-text').text,
       location: list.css('span.ts-quiet').text,
       warning: list.css('span.tag').text,
-      # url: "https://www.tokyodev.com" + list.css('a.job-title')[0].attributes['href'].value
+
     }
     unless job[:title] == ""
       jobs << job
     end
   end
   jobs.each do |job|
-    puts "#{job[:title]}, #{job[:company]}, #{job[:location]}, #{job[:warning]}"
+    puts "#{job[:title]}, #{job[:company]}, #{job[:location]}, #{job[:warning]}, #{job[:url]}" +
+      'url:' "https://www.tokyodev.com/companies/#{job[:company]}"
+
   end
 end
+
 scraper
