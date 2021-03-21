a0eff646c65c210d27bb09e0d3314cd86f623623
Adriana Ito <ito.dri@gmail.com>
2021-03-21 20:03:39 +0900

    add filter japanese not required

--- app.rb
+++ app.rb
@@ -3,24 +3,29 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-get '/' do
-  def scraper
-    url = 'https://www.tokyodev.com/'
-    unparsed_page = HTTParty.get(url)
-    data = Nokogiri::HTML(unparsed_page.body)
-    @jobs = []
-    @job_listings = data.css('div.jobs-list-item')
-    @job_listings.each do |list|
-      job = {
-        title: list.css('a.job-title').text,
-        company: list.css('div.media-object-text').text,
-        location: list.css('span.ts-quiet').text,
-        warning: list.css('span.tag').text,
-        url: list.css('div.media-object-text').text.downcase
-      }
-      @jobs << job unless job[:title] == ""
-    end
+def scraper
+  url = 'https://www.tokyodev.com/'
+  unparsed_page = HTTParty.get(url)
+  data = Nokogiri::HTML(unparsed_page.body)
+  @jobs = []
+  @job_listings = data.css('div.jobs-list-item')
+  @job_listings.each do |list|
+    job = {
+      title: list.css('a.job-title').text,
+      company: list.css('div.media-object-text').text,
+      location: list.css('span.ts-quiet').text,
+      warning: list.css('span.tag').text,
+      url: list.css('div.media-object-text').text.downcase
+    }
+    @jobs << job unless job[:title] == ""
   end
+end
+get '/' do
   scraper
   erb :show
+end
+
+get '/tag' do
+  scraper
+  erb :tag
 end
